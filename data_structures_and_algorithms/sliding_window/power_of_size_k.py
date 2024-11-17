0fc553b78e9132e8f993c291481ae71454cb0e6b
MissTipo <dorine.a.tipo@gmail.com>
2024-11-17 08:10:12 +0300

    Find the power of k subarrays

---
+++ data_structures_and_algorithms/sliding_window/power_of_size_k.py
@@ -0,0 +1,64 @@
+"""
+You are given an array of integers nums of length n and a positive integer k.
+
+The power of an array is defined as:
+
+Its maximum element if all of its elements are consecutive and sorted in ascending order.
+-1 otherwise.
+You need to find the power of all 
+subarrays
+ of nums of size k.
+
+Return an integer array results of size n - k + 1, where results[i] is the power of nums[i..(i + k - 1)].
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,3,2,5], k = 3
+
+Output: [3,4,-1,-1,-1]
+
+Explanation:
+
+There are 5 subarrays of nums of size 3:
+
+[1, 2, 3] with the maximum element 3.
+[2, 3, 4] with the maximum element 4.
+[3, 4, 3] whose elements are not consecutive.
+[4, 3, 2] whose elements are not sorted.
+[3, 2, 5] whose elements are not consecutive.
+Example 2:
+
+Input: nums = [2,2,2,2,2], k = 4
+
+Output: [-1,-1]
+
+Example 3:
+
+Input: nums = [3,2,3,2,3,2], k = 2
+
+Output: [-1,3,-1,3,-1]
+"""
+from typing import List
+def resultsArray(nums: List[int], k: int) -> List[int]:
+    def isConsecutive(arr):
+        for i in range(1, len(arr)):
+            if arr[i] != arr[i - 1] + 1:
+                return False
+        return True
+
+    n = len(nums)
+    result = []
+
+    for i in range(n - k + 1):
+        sub = nums[i:i + k]
+        if isConsecutive(sub):
+            result.append(max(sub))
+        else:
+            result.append(-1)
+    return result
+
+nums = [1,2,3,4,3,2,5]
+k = 3
+print(resultsArray(nums, k))
